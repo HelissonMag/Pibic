0d671cc313a658918b0ebf379380ae254fb0e6ab
Helisson Magalhaes <47993832+HelissonMag@users.noreply.github.com>
2019-03-07 19:18:22 -0300

    Update ImplementacaoAprend.java

--- ImplementacaoAprend.java
+++ ImplementacaoAprend.java
@@ -1,6 +1,7 @@
 ﻿
        
      
+           public static int[][] criar_rede(int no , int amigo){
            List<Integer> numeros;
             numeros = new ArrayList();
             for (int p = 0; p <no ; p++) { 
@@ -56,6 +57,57 @@
        }
           
        
-      
-    
-}
+       public static int[][] gerar_solucoes(int no, int NCidade,int[] demanda, int capacidade){
+            List<Integer> numeros,rota, adicionados;
+            int var;
+            int capTotal=0;
+            int[][] solucoes=new int[no][];
+            numeros = new ArrayList();
+            rota =new ArrayList();
+            adicionados=new ArrayList();
+            for (int p = 1; p <=NCidade ; p++) {
+                numeros.add(p);
+            }
+
+            for (int i = 0; i < no; i++) {
+                rota.add(0);
+                Collections.shuffle(numeros);  
+                for (int j = 0; j < numeros.size(); j++) {
+                
+                }
+                for (int j = 0; j < NCidade; j++) {
+                    var=numeros.get(j);
+                        if(demanda[var-1]+capTotal<=capacidade && !(adicionados.contains(var))){
+                           rota.add(var);
+                           adicionados.add(var);
+//                   System.out.println("Valor do adicionado "+adicionados.get(cont));
+                           capTotal=demanda[var-1]+capTotal;
+                         
+                           if(capTotal==capacidade){
+                 
+                               j=NCidade-1;
+                           }
+                          
+                        } 
+                   int v=NCidade-1;
+                     if(adicionados.size()<NCidade && j==(v)){
+                        rota.add(0);
+                        j=0;
+                        capTotal=0;
+                     }
+                      
+                }
+              
+                capTotal=0; 
+                rota.add(0);
+                solucoes[i]= new int[rota.size()];
+                int z=0;
+                while(z<rota.size()){
+                    solucoes[i][z]=rota.get(z);
+                    z++;
+                }
+                rota.clear();
+                adicionados.clear();
+           }
+            return solucoes;
+       }
